d5a4536d2b31977fffb20fd77f560677def11392
kafz <ramirofiscella@gmail.com>
2024-07-24 18:42:03 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
+<<<<<<< HEAD
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
@@ -7,3 +8,7 @@ export default defineConfig({
   site: 'http://localhost:4321/',
   integrations: [preact()]
 });
+=======
+// https://astro.build/config
+export default defineConfig({});
+>>>>>>> 4a8bb4a (first commit)
