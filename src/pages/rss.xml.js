5e6cd50766f8fafbcbc2671ccb6d9a8f62418bf3
kafz <ramirofiscella@gmail.com>
2024-09-09 11:26:24 -0300

    finishing

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'UNNAME | Blog',
-    description: 'UNNAME',
+    title: 'memo | Blog',
+    description: 'memo',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>es-la</language>`,
