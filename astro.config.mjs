bba0e42036cc3049f33ecb6c3949299bb6f35a8b
kafz <ramirofiscella@gmail.com>
2024-09-09 15:34:03 -0300

    Fix package.json syntax

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-<<<<<<< HEAD
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321/',
-  integrations: [preact()]
+  integrations: [preact()],
 });
-=======
-// https://astro.build/config
-export default defineConfig({});
->>>>>>> 4a8bb4a (first commit)
